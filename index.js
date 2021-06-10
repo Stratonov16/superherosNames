2f4ab9a80f21d3c9fe648ed2fa3d670ea9ad18bc
Nikhil <70238901+Stratonov16@users.noreply.github.com>
2021-06-10 13:31:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+//  author: Nikhil
+var superhero = require('superheroes');
+var supervillain = require('supervillains');
+var hero = superhero.random();
+var villain = supervillain.random();
+console.log(hero);
+console.log(villain);
